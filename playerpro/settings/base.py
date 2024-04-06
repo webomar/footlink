9ddf7df542f9edc91dfddcdcc862e7988053dafd
s1189750 <s1189750@student.windesheim.nl>
2024-04-06 19:48:48 +0200

    po

--- playerpro/settings/base.py
+++ playerpro/settings/base.py
@@ -108,24 +108,24 @@ WSGI_APPLICATION = "playerpro.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         #'ENGINE': 'django.db.backends.sqlite3',
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'E6aDDa365D4eF6ec3Caa-bB6BC5f1gab',
-#         'HOST': 'viaduct.proxy.rlwy.net',
-#         'PORT': '33811',
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'E6aDDa365D4eF6ec3Caa-bB6BC5f1gab',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '33811',
+    }
+}
 
 
 # Password validation
